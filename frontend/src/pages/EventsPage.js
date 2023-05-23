3cb73d9b34dbce2ca985a695e6cd66e6c75b2c6e
KacperLudwiczak <kacperludwiczak@o2.pl>
2023-05-23 14:16:42 +0200

    Dummy events to EventsPage

--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -1,5 +1,23 @@
+import { Link } from "react-router-dom";
+
+const DUMMY_EVENTS = [
+  { id: "e1", title: "Some event" },
+  { id: "e2", title: "Another event" },
+];
+
 function EventPage() {
-  return <h1>Events Page</h1>;
+  return (
+    <>
+      <h1>Events Page</h1>
+      <ul>
+        {DUMMY_EVENTS.map((event) => (
+          <li key={event.id}>
+            <Link to={event.id}>{event.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default EventPage;
