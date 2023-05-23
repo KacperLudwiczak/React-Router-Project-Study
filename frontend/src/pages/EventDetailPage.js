19c46451a90a677b18527dd6c51302b6e6c7855e
KacperLudwiczak <kacperludwiczak@o2.pl>
2023-05-23 14:19:14 +0200

    useParams

--- frontend/src/pages/EventDetailPage.js
+++ frontend/src/pages/EventDetailPage.js
@@ -1,5 +1,14 @@
+import { useParams } from "react-router-dom";
+
 function EventDetailPage() {
-  return <h1>Event Detail Page</h1>;
+  const params = useParams();
+
+  return (
+    <>
+      <h1>Event Detail Page</h1>
+      <p>Event ID: {params.eventId}</p>
+    </>
+  );
 }
 
 export default EventDetailPage;
