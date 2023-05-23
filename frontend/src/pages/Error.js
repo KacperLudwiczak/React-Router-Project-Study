c2680314845226abbc101ce25ec66ae93918e676
KacperLudwiczak <kacperludwiczak@o2.pl>
2023-05-23 16:12:33 +0200

    Error

--- frontend/src/pages/Error.js
+++ frontend/src/pages/Error.js
@@ -1,10 +1,28 @@
+import { useRouteError } from "react-router-dom";
 import PageContent from "../components/PageContent";
+import MainNavigation from "../components/MainNavigation";
 
 function ErrorPage() {
+  const error = useRouteError();
+
+  let title = "Error occured!";
+  let message = "Something went wrong!";
+
+  if (error.status === 500) {
+    message = JSON.parse(error.data).message;
+  }
+  if (error.status === 404) {
+    title = "Not found";
+    message = "Could not find page.";
+  }
+
   return (
-    <PageContent title="Error occured!">
-      <p>Something went wrong!</p>
-    </PageContent>
+    <>
+      <MainNavigation />
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
+    </>
   );
 }
 
