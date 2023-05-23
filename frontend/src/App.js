ff7a4db720a63d14bdb146a0381a82b35e4727b6
KacperLudwiczak <kacperludwiczak@o2.pl>
2023-05-23 14:36:58 +0200

    Event Page Change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,3 @@
-// 5. Ensure that the links in MainNavigation receive an "active" class when active
-// 6. Output a list of dummy events to the EventsPage
-//    Every list item should include a link to the respective EventDetailPage
-// 7. Output the ID of the selected event on the EventDetailPage
-// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
-
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import EventPage from "./pages/EventsPage";
