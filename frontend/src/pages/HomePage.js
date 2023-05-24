237def5f2f2f6e43d4d8e3e4fc6152065234d545
KacperLudwiczak <kacperludwiczak@o2.pl>
2023-05-24 15:46:55 +0200

    Finish

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,5 +1,10 @@
-function HomePage() {
-  return <h1>Home Page</h1>;
-}
+import PageContent from "../components/PageContent";
 
+function HomePage() {
+  return (
+    <PageContent title="Welcome!">
+      <p>Browse all our amazing events</p>
+    </PageContent>
+  );
+}
 export default HomePage;
